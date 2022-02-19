9cfb7bca894f4ec1ca0f87b64a60d50d503ca6f8
stlp <hfcheung@cs.washington.edu>
2022-02-18 23:01:23 -0800

    Commit skeleton code for website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <span className="title">CODE FOR EVERYONE</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
