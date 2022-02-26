b7f278349e0f07c7b2d3547b06a335151af36dde
stlp <hfcheung@cs.washington.edu>
2022-02-25 22:36:37 -0800

    Added links to pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="header">
         <span className="title">CODE FOR EVERYONE</span>
+        <Link to="/about">About</Link>
+        <Link to="/locations">Locations</Link>
+        <Link to="/resources">Resources</Link>
       </header>
     </div>
   );
